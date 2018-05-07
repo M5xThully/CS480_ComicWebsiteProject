8220bd2ed90931b803a2437e1f100ba9b51eaaa0
opt465 <allenlam465@gmail.com>
2018-05-07 21:38:48 +0000

    Added forms.py.

---
+++ comicsite/forms.py
@@ -0,0 +1,11 @@
+
+from django import forms
+from comicsite.models import Account
+
+class AccountForm(forms.ModelForm):
+	accountfirstname = form.CharField(label='First Name'
+	accountlastname
+	accountemail
+	accountusername
+	accountpassword
+	accountcity
