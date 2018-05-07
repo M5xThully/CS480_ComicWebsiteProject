db3fed7320f9d71897a249e074b30f5fd0658c6d
Allen Lam <allenlam465@gmail.com>
2018-05-07 15:44:23 -0700

    Needed to pull.

--- comicsite/forms.py
+++ comicsite/forms.py
@@ -3,8 +3,8 @@ from django import forms
 from comicsite.models import Account
 
 class AccountForm(forms.ModelForm):
-	accountfirstname = form.CharField(label='First Name'
-	accountlastname
+	accountfirstname = form.CharField(label='First Name')
+	accountlastname = form.CharField(label='Last Name')
 	accountemail
 	accountusername
 	accountpassword
