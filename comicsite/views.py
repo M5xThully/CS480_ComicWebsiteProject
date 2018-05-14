64f5e7a074899a7d3183535d67fadde4040c3eb1
Nathan <master555x@gmail.com>
2018-05-13 20:18:45 -0700

    Update views.py

--- comicsite/views.py
+++ comicsite/views.py
@@ -1,62 +1,77 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django import forms
 from comicsite.models import Comic
 from comicsite.models import Account
-from comicsite.models import Comment
-from comicsite.forms import UserForm, UserProfileForm
-# from rango.bing_search import run_query
+from comicsite.forms import AccountForm
+from comicsite.forms import UserForm
+from comicsite.forms import UserProfileForm
+from django.urls import reverse
+
 
 def home(request):
     return render(request, 'frontpage.html')
 
 
 def login(request):
-    return render(request, 'loginpage.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            print("Invalid login credentials: {0}, {1}".format(username, password))
+            return HttpResponse("Wrong username or password.")
+    else:
+        return render(request, 'loginpage.html', {})
 
 
 def registered(request):
     return render(request, 'registered.html')
 
-'''
-def register(request):
-    if request.method == 'POST':
-        form = AccountForm(request.POST)
-        if form.is_valid():
-            new_entry = form.save()
-            return redirect('/registered')
-    else:
-        form = AccountForm()
 
-    return render(request, 'registerpage.html', {'form':form})
-'''
 def register(request):
+    isregistered = False
     if request.method == 'POST':
         user_form = UserForm(request.POST)
         profile_form = UserProfileForm(request.POST)
         if user_form.is_valid() and profile_form.is_valid():
+
             user = user_form.save()
             user.set_password(user.password)
             user.save()
-            
+
             profile = profile_form.save(commit=False)
             profile.user = user
-            
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
             profile.save()
-            registered = True
-            
+
+            isregistered = True
+
             return redirect('/registered')
-            
+
         else:
-            # Invalid form or forms - mistakes or something else?
-            # Print problems to the terminal.
             print(user_form.errors, profile_form.errors)
+
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
 
-    return render(request, 'registerpage.html', {'form':UserForm}, {'form':UserProfileForm})
-
+    return render(request,
+                  'registerpage.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
 
 
 def user(request):
@@ -64,9 +79,7 @@ def user(request):
 
 
 def comic(request, pageid):
-
-    comic = Comic.objects.filter(comicid = pageid)[0]
-    comment_list = Comment.objects.filter(comicid = pageid).order_by('-date')[:5]
+    comic = Comic.objects.filter(comicid=pageid)[0]
 
     context_dict = {'title': comic.comictitle,
                     'id': comic.comicid,
@@ -79,13 +92,11 @@ def comic(request, pageid):
                     'rating': comic.comicrating,
                     'synopsis': comic.comicsynopsis,
                     'plot': comic.comicplot,
-		            'cover': comic.comiccover,
-                    'comments': comment_list}
+                    'cover': comic.comiccover}
     return render(request, 'comicpage.html', context_dict)
 
 
 def account(request, userid):
-
     account = Account.objects.filter(accountid=userid)[0]
 
     context_dict = {'id': account.accountid,
@@ -100,14 +111,3 @@ def account(request, userid):
 
     return render(request, 'user.html', context_dict)
 #    return render(request, 'user.html')
-
-
-def search(request):
-    result_list = []
-    if request.method == 'POST':
-        query = request.POST['query'].strip()
-        if query:
-            # Run our Bing function to get the results list!
-            result_list = run_query(query)
-    return render(request, 'rango/search.html', {'result_list': result_list})
-
