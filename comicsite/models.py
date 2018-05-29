6a398952204c983d0f615b9b785a6f460d00e01e
Nathan <master555x@gmail.com>
2018-05-28 20:11:41 -0700

    Update to date field in post.

--- comicsite/models.py
+++ comicsite/models.py
@@ -147,7 +147,7 @@ class Post(models.Model):
     # the user posting the post
     userid = models.IntegerField(null=False)
     # date of the post
-    date = models.DateTimeField(auto_now=True, null=False)
+    date = models.DateTimeField(auto_now_add=True, null=False)
 
     class Meta:
         managed = False
